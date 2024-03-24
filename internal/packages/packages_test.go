b6ef9bc4e5441b61290b0610ef4995a020cbb366
Jared Allard <jaredallard@users.noreply.github.com>
2024-03-24 10:20:54 +0700

    packages: add test

---
+++ internal/packages/packages_test.go
@@ -0,0 +1,21 @@
+package packages_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jaredallard/binhost/internal/packages"
+	"gotest.tools/v3/assert"
+)
+
+func TestCanParseGpkg(t *testing.T) {
+	f, err := os.Open("testdata/onepassword-cli-0-1.gpkg.tar")
+	assert.NilError(t, err)
+
+	pkg, err := packages.New(f)
+	assert.NilError(t, err)
+
+	// Check that one field is set. Maybe one day check them all (I'm
+	// lazy)
+	assert.Equal(t, "onepassword-cli-0", pkg.PF)
+}
